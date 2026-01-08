aee02607a0b166359f3a3ca0f109d5f8eb54e2b5
Asfahan <asfhanarif@gmail.com>
2026-01-08 15:31:27 +0000

    still it's not loading, use logo url: https://i.ibb.co/5g03Zq7C/MC-logo.

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("relative h-32 w-48 md:h-40 md:w-60 transition-transform duration-300 hover:scale-105", className)}>
       <Image
-        src="https://i.ibb.co/68Z1Jd5/image.png"
+        src="https://i.ibb.co/5g03Zq7C/MC-logo.png"
         alt="Muslimahs Club Logo"
         layout="fill"
         className="object-contain"
