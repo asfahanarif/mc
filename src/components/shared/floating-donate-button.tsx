9c5cb82770f493b2ec3082ee6a5e55bcc5d93aeb
Asfahan <asfhanarif@gmail.com>
2026-01-08 19:07:05 +0000

    can we remove heart icon from donate button in mobile devices?

--- src/components/shared/floating-donate-button.tsx
+++ src/components/shared/floating-donate-button.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Heart } from "lucide-react";
 import { useIsMobile } from "@/hooks/use-mobile";
 
 export function FloatingDonateButton() {
@@ -16,7 +15,6 @@ export function FloatingDonateButton() {
         <div className="fixed bottom-4 right-4 z-50 md:hidden">
             <Button asChild size="lg" className="rounded-full shadow-lg group transition-all duration-300 ease-in-out hover:scale-105">
                 <Link href="/donate">
-                    <Heart className="mr-2 h-5 w-5 transition-transform duration-300 group-hover:scale-125 group-hover:fill-red-500 text-red-500" />
                     Donate
                 </Link>
             </Button>
