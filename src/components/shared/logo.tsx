284fc10dd69c442c9892710a10a86caa91f5b802
Asfahan <asfhanarif@gmail.com>
2026-01-08 15:23:17 +0000

    this is our logo, use this

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("relative h-32 w-48 md:h-40 md:w-60 transition-transform duration-300 hover:scale-105", className)}>
       <Image
-        src="https://i.ibb.co/5g03Zq7/MC-logo.png"
+        src="https://i.ibb.co/68Z1Jd5/image.png"
         alt="Muslimahs Club Logo"
         layout="fill"
         className="object-contain"
