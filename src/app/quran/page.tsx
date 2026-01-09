00cdeea7a964c15e37e8de1b21a86922f46fa5e9
Asfahan <asfhanarif@gmail.com>
2026-01-09 08:41:06 +0000

    make it floating round dialog

--- src/app/quran/page.tsx
+++ src/app/quran/page.tsx
@@ -197,7 +197,7 @@ export default function QuranPage() {
       <Dialog open={!!activeSurah} onOpenChange={(isOpen) => !isOpen && handleDialogClose()}>
         <DialogContent className={cn(
             "max-w-4xl w-[calc(100vw-2rem)] h-auto max-h-[80vh] p-0 flex flex-col",
-            "bg-background/95 backdrop-blur-sm"
+            "bg-background/95 backdrop-blur-sm sm:rounded-lg"
         )}>
            {activeSurah && (
                 <>
