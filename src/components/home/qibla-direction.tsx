65a97b6de3b1917115cd0bb218c996366c122318
Asfahan <asfhanarif@gmail.com>
2026-01-14 20:26:32 +0000

    can we hide closer button

--- src/components/home/qibla-direction.tsx
+++ src/components/home/qibla-direction.tsx
@@ -199,10 +199,6 @@ export function QiblaDirection() {
                             A compass to help you find the direction of the Qibla for prayer.
                         </DialogDescription>
                     </DialogHeader>
-                    <DialogClose className="absolute right-4 top-4 rounded-full opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none z-10 bg-background/50 h-8 w-8">
-                        <X className="h-4 w-4 mx-auto" />
-                        <span className="sr-only">Close</span>
-                    </DialogClose>
                     {renderContent()}
                 </div>
             </DialogContent>
