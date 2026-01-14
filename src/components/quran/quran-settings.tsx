0ecc5063ba574aa7241fc775e99d3ed11ae2920e
Asfahan <asfhanarif@gmail.com>
2026-01-14 04:35:47 +0000

    okay then replace tajawal with some other classic

--- src/components/quran/quran-settings.tsx
+++ src/components/quran/quran-settings.tsx
@@ -152,7 +152,7 @@ export function QuranSettings({ allTranslations, allReciters, settingType }: { a
                   <SelectItem value="'Noto Nastaliq Urdu', serif">Noto Nastaliq Urdu</SelectItem>
                   <SelectItem value="'Amiri', serif">Amiri</SelectItem>
                   <SelectItem value="'Scheherazade New', serif">Scheherazade New</SelectItem>
-                  <SelectItem value="'Tajawal', sans-serif">Tajawal</SelectItem>
+                  <SelectItem value="'Lateef', serif">Lateef</SelectItem>
                   <SelectItem value="'Almarai', sans-serif">Almarai</SelectItem>
               </SelectContent>
             </Select>
