20718bfec655512ca05ed53ef09914bcb2f67125
Asfahan <asfhanarif@gmail.com>
2026-01-14 08:44:18 +0000

    give extra more padding

--- src/components/quran/quran-reader.tsx
+++ src/components/quran/quran-reader.tsx
@@ -330,7 +330,7 @@ export function QuranReader({ surah, allSurahs, allTranslations, onClose, onSura
 
       {/* Main Content */}
       <ScrollArea className="flex-grow" viewportRef={scrollContainerRef} onScroll={handleScroll}>
-        <div className="container max-w-4xl pt-8 pb-40">
+        <div className="container max-w-4xl pt-8 pb-64">
           {loadingDetails && (
             <div className="space-y-6">
               {[...Array(5)].map((_, i) => <Skeleton key={i} className="h-24 w-full" />)}
