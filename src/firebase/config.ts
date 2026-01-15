3f1b4e85180d144f7f02775b90fe3ce3a185ee43
Asfahan <asfhanarif@gmail.com>
2026-01-15 15:53:36 +0000

    i wanna deploy it into vercel, make it deployable, fix all the errors an

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-7376893633-29e80",
-  "appId": "1:254218581756:web:6674194f0339a1007e0979",
-  "apiKey": "AIzaSyCfxN6TPIuf2VEM-9DFI2Cl2N9i3i7uSmg",
-  "authDomain": "studio-7376893633-29e80.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "254218581756"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
